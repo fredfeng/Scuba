dd077c684bf21e5d0ef28fe9219ae13e3ff31a3c
xwangsd <xinyuwangsd@gmail.com>
2014-06-01 15:36:55 -0500

    before merge

--- src/framework/scuba/analyses/dataflow/IntraProcSumAnalysis.java
+++ src/framework/scuba/analyses/dataflow/IntraProcSumAnalysis.java
@@ -14,104 +14,121 @@ import framework.scuba.utils.Graph;
 import framework.scuba.utils.Node;
 
 /**
- * Intra-proc summary-based analysis
- * Check the rules in Figure 8 of our paper.
+ * Intra-proc summary-based analysis Check the rules in Figure 8 of our paper.
+ * 
  * @author yufeng
- *
+ * 
  */
-public class IntraProcSumAnalysis {  
-    
-    Summary summary; 
+public class IntraProcSumAnalysis {
 
-    public void analyze(ControlFlowGraph g) {
-    	BasicBlock entry = g.entry();
-    	HashSet<BasicBlock> roots = new HashSet();
-    	HashMap<Node, Set<BasicBlock>> nodeToScc = new HashMap();
-    	HashMap<Set<BasicBlock>, Node> sccToNode = new HashMap();
-    	HashMap<BasicBlock, Node> bbToNode = new HashMap();
+	Summary summary;
 
+	public void analyze(ControlFlowGraph g) {
+		BasicBlock entry = g.entry();
+		HashSet<BasicBlock> roots = new HashSet();
+		HashMap<Node, Set<BasicBlock>> nodeToScc = new HashMap();
+		HashMap<Set<BasicBlock>, Node> sccToNode = new HashMap();
+		HashMap<BasicBlock, Node> bbToNode = new HashMap();
 
-    	roots.add(entry);
-    	Graph repGraph = new Graph();
-    	SCCHelper sccManager = new SCCHelper(g, roots);
-    	System.out.println("SCC List in BBs:-----" + sccManager.getComponents());
-    	int idx = 0;
-    	//compute SCC in current CFG.
-    	for(Set<BasicBlock> scc : sccManager.getComponents()){
-    		//create a representation node for each scc.
-    		idx++;
-    		Node node = new Node("scc" + idx);
-    		nodeToScc.put(node, scc);
-    		sccToNode.put(scc, node);
-    		for(BasicBlock mb : scc) 
-    			bbToNode.put(mb, node);
-    		
-    		repGraph.addNode(node);
-    		if(scc.contains(entry))
-    			repGraph.setEntry(node);
-    	}
-    	
-    	for(Set<BasicBlock> scc : sccManager.getComponents()){
-    		Node cur = sccToNode.get(scc);
-    		for(BasicBlock nb : scc) {
-    			for(BasicBlock sucb : nb.getSuccessors()) {
-    				if(scc.contains(sucb))
-    					continue;
-    				else {
-    					Node scNode = bbToNode.get(sucb);
-    					cur.addSuccessor(scNode);
-    				}
-    			}
-    		}
-    	}
-    	
-    	for(Node rep : repGraph.getReversePostOrder()) {
-    		Set<BasicBlock> scc = nodeToScc.get(rep);
-    		if(scc.size() == 1) {
-    			BasicBlock sccB = scc.iterator().next();
-    			//self loop in current block.
-    			if(sccB.getSuccessors().contains(sccB))
-    				handleSCC(scc);
-    			else
-    				this.handleBasicBlock(sccB);
-    		} else {
+		roots.add(entry);
+		Graph repGraph = new Graph();
+		SCCHelper sccManager = new SCCHelper(g, roots);
+		System.out
+				.println("SCC List in BBs:-----" + sccManager.getComponents());
+		int idx = 0;
+		// compute SCC in current CFG.
+		for (Set<BasicBlock> scc : sccManager.getComponents()) {
+			// create a representation node for each scc.
+			idx++;
+			Node node = new Node("scc" + idx);
+			nodeToScc.put(node, scc);
+			sccToNode.put(scc, node);
+			for (BasicBlock mb : scc)
+				bbToNode.put(mb, node);
+
+			repGraph.addNode(node);
+			if (scc.contains(entry))
+				repGraph.setEntry(node);
+		}
+
+		for (Set<BasicBlock> scc : sccManager.getComponents()) {
+			Node cur = sccToNode.get(scc);
+			for (BasicBlock nb : scc) {
+				for (BasicBlock sucb : nb.getSuccessors()) {
+					if (scc.contains(sucb))
+						continue;
+					else {
+						Node scNode = bbToNode.get(sucb);
+						cur.addSuccessor(scNode);
+					}
+				}
+			}
+		}
+
+		for (Node rep : repGraph.getReversePostOrder()) {
+			Set<BasicBlock> scc = nodeToScc.get(rep);
+			if (scc.size() == 1) {
+				BasicBlock sccB = scc.iterator().next();
+				// self loop in current block.
+				if (sccB.getSuccessors().contains(sccB))
+					handleSCC(scc);
+				else
+					this.handleBasicBlock(sccB);
+			} else {
 				handleSCC(scc);
-    		}
-    	}
-    }
-    
-	//compute the fixed-point for this scc.
-    public void handleSCC(Set<BasicBlock> scc) {
-    	LinkedList<BasicBlock> wl = new LinkedList();
-    	HashMap<BasicBlock, Boolean> visit = new HashMap();
-    	for(BasicBlock b : scc)
-    		visit.put(b, false);
-    	
-    	wl.addAll(scc);
-    	while(true) {
-    		BasicBlock bb = wl.poll();
-    		boolean flag = handleBasicBlock(bb);
-    		if(flag) 
-    			for(BasicBlock b : scc)
-    				visit.put(b, false);
-    		else
-    			visit.put(bb, true);
-    		boolean allTrue = !visit.values().contains(false);
-    		
-    		if(allTrue) break;
-    	}
-    }
-    
-    public boolean handleBasicBlock(BasicBlock bb) {
-    	summary.getAbstractHeap().isChanged = false;
-    	//handle each quad in the basicblock.
-    	for(Quad q : bb.getQuads())
-    		summary.handleStmt(q);
-    	
-    	return summary.getAbstractHeap().isChanged;
-    }
-    
-    public void setSummary(Summary sum) {
-    	summary = sum;
-    }
+			}
+		}
+	}
+
+	// compute the fixed-point for this scc.
+	public void handleSCC(Set<BasicBlock> scc) {
+		LinkedList<BasicBlock> wl = new LinkedList();
+		HashMap<BasicBlock, Boolean> visit = new HashMap();
+		for (BasicBlock b : scc)
+			visit.put(b, false);
+
+		wl.addAll(scc);
+		while (true) {
+			BasicBlock bb = wl.poll();
+			boolean flag = handleBasicBlock(bb);
+			if (flag)
+				for (BasicBlock b : scc)
+					visit.put(b, false);
+			else
+				visit.put(bb, true);
+			boolean allTrue = !visit.values().contains(false);
+
+			if (allTrue)
+				break;
+		}
+	}
+
+	public boolean handleBasicBlock(BasicBlock bb) {
+
+		System.out.println("=========================");
+		System.out.println("Handling the basic block: ");
+		System.out.println(bb);
+
+		summary.getAbstractHeap().isChanged = false;
+		// handle each quad in the basicblock.
+		for (Quad q : bb.getQuads()) {
+			
+			System.out.println("-------------------------");
+			System.out.println("Handling the statement: ");
+			System.out.println(q);
+
+			summary.handleStmt(q);
+
+			System.out.println("Finish handling the statement.");
+			System.out.println("-------------------------");
+		}
+
+		System.out.println("Finish handling the basic block.");
+		System.out.println("=========================");
+		return summary.getAbstractHeap().isChanged;
+	}
+
+	public void setSummary(Summary sum) {
+		summary = sum;
+	}
 }
