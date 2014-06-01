9f310818c02b45a852c44e0c629e57e242b1d5fc
xwangsd <xinyuwangsd@gmail.com>
2014-06-01 15:30:22 -0500

    change test rule 6

--- src/test/intraproc/TestRule6.java
+++ src/test/intraproc/TestRule6.java
@@ -1,23 +1,28 @@
 package test.intraproc;
 
-
 /**
  * Test case for Rule 4 in figure 8.
+ * 
  * @author yufeng
- *
+ * 
  */
 public class TestRule6 {
-	
+
 	class Z {
 	}
-	
-	//if-else.
-	void foo(Z a) {
+
+	// if-else.
+	Z foo(Z a) {
 		int i = 1;
-		Z v,u;
-		if(i > 0)	
+		Z v, u, ret;
+		if (i > 0) {
 			v = new Z();
-		else
+			ret = v;
+		} else {
 			u = new Z();
+			ret = u;
+		}
+
+		return ret;
 	}
 }
