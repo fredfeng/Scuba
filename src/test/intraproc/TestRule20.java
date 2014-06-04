5f4d7e1cfd7718933e409e4abdc55bce79e63cb1
Yu Feng <fengyu8299@gmail.com>
2014-06-03 23:08:58 -0500

    arg test case

--- src/test/intraproc/TestRule20.java
+++ src/test/intraproc/TestRule20.java
@@ -20,16 +20,24 @@ public class TestRule20 {
 	}
 	
 	class F {
-		
+		F f;
 	}
 
-	void foo() {
+	void foo(X[] array, F arg) {
+		X a = array[1];
 		X x = new Y();
 		x.f = new F();
 		F f0 = x.f;
 
+		x.f = arg;
+		x = a;
+		a.f = new F();
+		
 		Y y = (Y)x;
 		F f = y.f;
+		y.f.f = a.f;
+		
+		arg = a.f;//arg to arg
 		
 		X z = new Z();
 		z.f = new F();
