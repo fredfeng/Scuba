8f13ec43c8108a41ef080f72b54efca56c3ac098
Yu Feng <fengyu8299@gmail.com>
2014-06-07 12:42:24 -0500

    Xinyu's alias exampel

--- src/test/interproc/TestInter2.java
+++ src/test/interproc/TestInter2.java
@@ -5,7 +5,29 @@ public class TestInter2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		new TestInter2().run();
 	}
 
+	void run() {
+		A x = new A();
+		A y = x;
+		foo(x, y);
+	}
+	
+	void foo(A x, A y){
+		int i = 0;
+		while(i < 10) {
+			i++;
+			B t = x.f;
+			y.f = new B();
+		}
+	}
+	
+	class A {
+		B f;
+	}
+	
+	class B{
+		
+	}
 }
