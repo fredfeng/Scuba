bb79fccf9d5f055f487ef858d26ede82c4833608
xwangsd <xinyuwangsd@gmail.com>
2014-06-07 15:51:52 -0500

    some dbg info in G()

--- src/framework/scuba/helper/G.java
+++ src/framework/scuba/helper/G.java
@@ -2,11 +2,19 @@ package framework.scuba.helper;
 
 /**
  * some global environments.
+ * 
  * @author yufeng
- *
+ * 
  */
 public class G {
-	public static boolean debug = false;
+
+	public static boolean debug = true;
+
+	public static boolean dump = true;
+
+	public static int count = 0;
+	
+	public static int step = 1;
 
 	public static String dotOutputPath = "/Users/xwang/xwang/Research/Projects/scuba/Scuba/output/";
 
