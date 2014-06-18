9932d2d52f2267f5ed238c5db7424ec36d7d1409
xwangsd <xinyuwangsd@gmail.com>
2014-06-17 23:57:23 -0500

    testing dacapo for cache without proplocal

--- src/framework/scuba/domain/SummariesEnv.java
+++ src/framework/scuba/domain/SummariesEnv.java
@@ -38,7 +38,7 @@ public class SummariesEnv {
 	protected int allocDepth = 1;
 
 	// prop locals or not
-	protected boolean propLocals = true;
+	protected boolean propLocals = false;
 
 	// ignore string
 	protected boolean openBlklist = false;
