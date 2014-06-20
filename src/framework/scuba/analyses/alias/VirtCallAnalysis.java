7ea46f0e29a1dd63f751600d935889672758ad8a
Yu Feng <fengyu8299@gmail.com>
2014-06-20 18:32:57 -0500

    stub analyses for experiment.

---
+++ src/framework/scuba/analyses/alias/VirtCallAnalysis.java
@@ -0,0 +1,14 @@
+package framework.scuba.analyses.alias;
+
+/**
+ * Analysis to resolve virtual callsites. 
+ * We are interested in the following:
+ * 1. Number of virtual calls that have single target;
+ * 2. Average number of callsites in App;
+ * 3. Maximum number of callsites in App.
+ */
+public class VirtCallAnalysis {
+	public VirtCallAnalysis() {
+
+	}
+}
