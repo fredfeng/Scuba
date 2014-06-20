b6ae36c6566d48bed2e8d8000a6d35d1fa5d9bef
xwangsd <xinyuwangsd@gmail.com>
2014-06-20 15:07:57 -0500

    G

--- src/framework/scuba/helper/G.java
+++ src/framework/scuba/helper/G.java
@@ -30,15 +30,15 @@ public class G {
 
 	public static boolean dbgPermission = true;
 
-	public static boolean dbgIsil = false;
-
 	public static boolean dbgQuery = false;
 
 	public static boolean instnInfo = true;
 
 	public static boolean dbgRef = false;
 
-	public static boolean seePerf = true;
+	public static boolean seePerf = false;
+
+	public static boolean dbgSmashing = false;
 
 	public static int sample = 155;
 
