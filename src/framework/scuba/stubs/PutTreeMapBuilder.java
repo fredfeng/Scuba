66922626a92992a7947edc2974c64899d4dfa880
Yu Feng <fengyu8299@gmail.com>
2014-06-25 15:20:33 -0500

    more stub methods

--- src/framework/scuba/stubs/PutTreeMapBuilder.java
+++ src/framework/scuba/stubs/PutTreeMapBuilder.java
@@ -1,15 +1,25 @@
 package framework.scuba.stubs;
 
+import joeq.Class.jq_Class;
+import joeq.Class.jq_Field;
 import joeq.Class.jq_Method;
+import joeq.Class.jq_NameAndDesc;
 import joeq.Class.jq_Type;
 import joeq.Compiler.Quad.BasicBlock;
 import joeq.Compiler.Quad.ControlFlowGraph;
 import joeq.Compiler.Quad.ICFGBuilder;
-import joeq.Compiler.Quad.Operand.IConstOperand;
+import joeq.Compiler.Quad.Operand.FieldOperand;
+import joeq.Compiler.Quad.Operand.MethodOperand;
+import joeq.Compiler.Quad.Operand.ParamListOperand;
+import joeq.Compiler.Quad.Operand.RegisterOperand;
+import joeq.Compiler.Quad.Operator.Getfield;
+import joeq.Compiler.Quad.Operator.Invoke;
+import joeq.Compiler.Quad.Operator.Putfield;
 import joeq.Compiler.Quad.Operator.Return;
-import joeq.Compiler.Quad.Operator.Return.RETURN_I;
+import joeq.Compiler.Quad.Operator.Return.RETURN_A;
 import joeq.Compiler.Quad.Quad;
 import joeq.Compiler.Quad.RegisterFactory;
+import joeq.Compiler.Quad.RegisterFactory.Register;
 
 /**
  * Stub for method "boolean put(k,v)" in
@@ -28,9 +38,63 @@ public class PutTreeMapBuilder implements ICFGBuilder {
     		rf.getOrCreateLocal(i, t);
 		}
 		
+		jq_Type objType = jq_Type.parseType("java.lang.Object");
+		jq_Type mapType = jq_Type.parseType("java.util.TreeMap");
+		jq_Type mapEntryType = jq_Type.parseType("java.util.TreeMap$Entry");
+		assert mapEntryType != null;
+
+		Register r0 = rf.getOrCreateLocal(0, mapType);
+		RegisterOperand ro0 = new RegisterOperand(r0, mapType);
+		//key
+		Register r1 = rf.getOrCreateLocal(1, objType);
+		RegisterOperand ro1 = new RegisterOperand(r1, objType);
+		//value
+		Register r2 = rf.getOrCreateLocal(2, objType);
+		RegisterOperand ro2 = new RegisterOperand(r2, objType);
+		
+		Register t2 = rf.getOrCreateStack(2, objType);
+		RegisterOperand to2 = new RegisterOperand(t2, mapEntryType);
+		
+		Register t3 = rf.getOrCreateStack(3, objType);
+		RegisterOperand to3 = new RegisterOperand(t3, objType);
+		
 		ControlFlowGraph cfg = new ControlFlowGraph(m, 1, 0, rf);
-		BasicBlock bb = cfg.createBasicBlock(1, 1, 1, null);
-		Quad ret = Return.create(1, bb, RETURN_I.INSTANCE, new IConstOperand(0));
+		BasicBlock bb = cfg.createBasicBlock(1, 1, 3, null);
+        mapEntryType.load();
+
+		jq_Field val = ((jq_Class)mapEntryType).getDeclaredField("value");
+		jq_Field key = ((jq_Class)mapEntryType).getDeclaredField("key");
+        jq_NameAndDesc ndOfRun = new jq_NameAndDesc("setValue", "(Ljava/lang/Object;)Ljava/lang/Object;");
+        jq_Method setVal = ((jq_Class)mapEntryType).getDeclaredInstanceMethod(ndOfRun);
+        MethodOperand mo = new MethodOperand(setVal);
+		jq_Field root = ((jq_Class)mapType).getDeclaredField("root");
+		assert(root != null);
+		assert(key != null);
+		assert(val != null);
+
+		FieldOperand foRoot = new FieldOperand(root);
+		FieldOperand foKey = new FieldOperand(key);
+		FieldOperand foVal = new FieldOperand(val);
+//		 GETFIELD_A T15, R0, .root
+		Quad q1 = Getfield.create(0, bb, Getfield.GETFIELD_A.INSTANCE, to2, ro0, foRoot, null);
+		
+//		Quad q2 = Getfield.create(0, bb, Getfield.GETFIELD_A.INSTANCE, to2, ro0, foRoot, null);
+
+//			3: PUTFIELD_A T15, .key, R1
+//		Quad q2 = Putfield.create(1, bb, Putfield.PUTFIELD_A.INSTANCE, to2, foKey, ro1, null);
+//			3: PUTFIELD_A T15, .value, R2
+//		Quad q3 = Putfield.create(2, bb, Putfield.PUTFIELD_A.INSTANCE, to2, foVal, ro2, null);
+        Quad q3 = Invoke.create(2, bb, Invoke.INVOKEVIRTUAL_V.INSTANCE, null, mo, setVal.getParamTypes().length);
+        RegisterOperand[] t = {to2,ro2};
+        ParamListOperand po = new ParamListOperand(t);
+        Invoke.setParamList(q3, po);
+		
+		Quad ret = Return.create(3, bb, RETURN_A.INSTANCE, ro2);
+		
+//		setValue:(Ljava/lang/Object;)Ljava/lang/Object;@java.util.TreeMap$Entry
+		bb.appendQuad(q1);
+//		bb.appendQuad(q2);
+		bb.appendQuad(q3);
 		bb.appendQuad(ret);
 		BasicBlock entry = cfg.entry();
 		BasicBlock exit = cfg.exit();
@@ -40,4 +104,5 @@ public class PutTreeMapBuilder implements ICFGBuilder {
 		exit.addPredecessor(bb);
 		return cfg;
 	}
+
 }
---
+++ src/framework/scuba/stubs/ToStringHtEntryBuilder.java
@@ -0,0 +1,56 @@
+package framework.scuba.stubs;
+
+import joeq.Class.jq_Method;
+import joeq.Class.jq_Type;
+import joeq.Compiler.Quad.BasicBlock;
+import joeq.Compiler.Quad.ControlFlowGraph;
+import joeq.Compiler.Quad.ICFGBuilder;
+import joeq.Compiler.Quad.Operand.RegisterOperand;
+import joeq.Compiler.Quad.Operand.TypeOperand;
+import joeq.Compiler.Quad.Operator.New;
+import joeq.Compiler.Quad.Operator.Return;
+import joeq.Compiler.Quad.Operator.Return.RETURN_A;
+import joeq.Compiler.Quad.Quad;
+import joeq.Compiler.Quad.RegisterFactory;
+import joeq.Compiler.Quad.RegisterFactory.Register;
+
+/**
+ * Stub for method "String toString()" in
+ * class java.lang.String.
+ * 
+ * @author Yu Feng (yufeng@cs.utexas.edu)
+ */
+public class ToStringHtEntryBuilder implements ICFGBuilder {
+	@Override
+	public ControlFlowGraph run(jq_Method m) {
+		jq_Type[] argTypes = m.getParamTypes();
+		int n = argTypes.length;
+		RegisterFactory rf = new RegisterFactory(0, n);
+		for (int i = 0; i < n; i++) {
+			jq_Type t = argTypes[i];
+    		rf.getOrCreateLocal(i, t);
+		}
+		
+		jq_Type strType = jq_Type.parseType("java.lang.String");
+		Register r1 = rf.getOrCreateStack(1, strType);
+
+		RegisterOperand ro = new RegisterOperand(r1, strType);
+
+		ControlFlowGraph cfg = new ControlFlowGraph(m, 1, 0, rf);
+		BasicBlock bb = cfg.createBasicBlock(1, 1, 1, null);
+		
+		Quad q1 = New.create(1, bb, New.NEW.INSTANCE, ro, new TypeOperand(strType));
+		Quad ret = Return.create(5, bb, RETURN_A.INSTANCE);
+		Return.setSrc(ret, ro);
+		
+		bb.appendQuad(q1);
+		bb.appendQuad(ret);
+		BasicBlock entry = cfg.entry();
+		BasicBlock exit = cfg.exit();
+		bb.addPredecessor(entry);
+		bb.addSuccessor(exit);
+		entry.addSuccessor(bb);
+		exit.addPredecessor(bb);
+		return cfg;
+	}
+}
