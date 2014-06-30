4bd520cdcab8d08e2085c98bef6a4bf0f00b5221
xwangsd <xinyuwangsd@gmail.com>
2014-06-30 17:47:28 -0500

    still

--- src/framework/scuba/helper/G.java
+++ src/framework/scuba/helper/G.java
@@ -30,7 +30,7 @@ public class G {
 	public static boolean dbgQuery = false;
 
 	public static int mId = 0;
-	public static boolean dbgAntlr = false;
+	public static boolean dbgAntlr = true;
 	public static boolean dbgInvoke = false;
 	public static boolean dbgInstn = false;
 	public static Map<Summary, Integer> IdMapping = new HashMap<Summary, Integer>();
